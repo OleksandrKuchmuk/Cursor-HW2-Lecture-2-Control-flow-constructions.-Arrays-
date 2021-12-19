209cc8e91f8a66029437e09c9e3eeb952c6591ce
Oleksandr Kuchmuk <okuchmuk@gmail.com>
2021-12-19 16:53:25 +0200

    Performed the main 10 tasks

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,7 +1,5 @@
 package com.company;
 
-import java.sql.SQLOutput;
-import java.util.Scanner;
 import java.util.Random;
 
 public class Main {
@@ -16,68 +14,104 @@ public class Main {
 
 //        2. Заповнити масив рандомними числами
 
-        for (int i=0; i<array.length; i++){
+        for (int i = 0; i < array.length; i++) {
             Random randomNumbers = new Random();
-            array[i]= randomNumbers.nextInt(500);
+            array[i] = randomNumbers.nextInt(500);
         }
 
 //        3. Вивести середнє значення масиву
         double count = 0;
-        for (int i : array){
-            count+=i;
+        for (int i : array) {
+            count += i;
         }
-        count/= array.length;
+        count /= array.length;
         System.out.println("The average value of the array: " + count);
         System.out.println();
 
 //        4. Вивести суму всіх значень кратних 3
         int countDivisoin3 = 0;
-        for (int i : array){
-            if (i%3 ==0){
-                countDivisoin3+=i;
+        for (int i : array) {
+            if (i % 3 == 0) {
+                countDivisoin3 += i;
             }
         }
-        System.out.println("The sum of all values multiples of 3: "+countDivisoin3);
+        System.out.println("The sum of all values multiples of 3: " + countDivisoin3);
         System.out.println();
 
 //        5. Вивести значення всіх парних індексів масиву
-        for (int i=0; i< array.length; i++){
-            if (i%2==0){
-                System.out.println("the value of the " + i+" array element:   " +array[i]);
+        for (int i = 0; i < array.length; i++) {
+            if (i % 2 == 0) {
+                System.out.println("The value of the " + i + " array element:   " + array[i]);
             }
         }
         System.out.println();
 
 //        6. Просортувати масив в порядку зростання
 //        Insertion sort
-        for (int i =1; i< array.length; ++i){
+        for (int i = 1; i < array.length; ++i) {
             int temp = array[i];
-            int j=i-1;
-            while (j>=0 && array[j]>temp) {
-            array[j+1]=array[j];
-            j=j-1;
-        }
-        array[j+1]=temp;
+            int j = i - 1;
+            while (j >= 0 && array[j] > temp) {
+                array[j + 1] = array[j];
+                j = j - 1;
+            }
+            array[j + 1] = temp;
 
         }
-//        System.out.println("Test output of the elements of the sorted array:");
-//        for (int i = 0; i < array.length; ++i) {
-//            System.out.print(array[i] + " ");
-//        }
-//        System.out.println();
-    }
+
+
+//        Test output of the elements of the sorted array
+
+        System.out.println("Test output of the elements of the sorted array:");
+        for (int i = 0; i < array.length; ++i) {
+            System.out.print(array[i] + " ");
+        }
+        System.out.println();
+        System.out.println();
 
 
 //        7. Вивести найменше значення в масиві
+        int smallestValue = 500;
+        for (int i = 0; i <= array.length - 1; i++) {
+            if (array[i] <= smallestValue) {
+                smallestValue = array[i];
+            }
+        }
+        System.out.println("The smallest value of the array:  " + smallestValue);
+        System.out.println();
 
 
 //        8. Вивести найбільше значення масиву
+        int largestValue = 0;
+        for (int i = 0; i <= array.length - 1; i++) {
+            if (array[i] >= largestValue) {
+                largestValue = array[i];
+            }
+        }
+        System.out.println("The largest value of the array:   " + largestValue);
+        System.out.println();
+
+
 //        9. Вивести суму значень всіх парних індексів
+        int summOfAllPairedIndices = 0;
+        for (int i = 0; i < array.length; i++) {
+            if (i % 2 == 0) {
+                summOfAllPairedIndices += array[i];
+            }
+        }
+        System.out.println("The sum of all paired indices:   " + summOfAllPairedIndices);
+        System.out.println();
+
 //        10. Вивести перших 20 символів з таблиці ASCI
+        System.out.println("The first 20 characters of the ASCI table:");
+        for (char i = 32; i <= 52; i++) {
+            System.out.print(i + " ");
+        }
 //
 //* (завдання з зірочкою)
 //        Заповнити масив рандомними числами, вивести їх, потім поміняти місцями парні з непарними
 
 
     }
+}
 
